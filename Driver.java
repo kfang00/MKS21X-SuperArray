b2d786caaec62adf918dbc767a807e0709cfa37b
Kayla Fang <kfang00@stuy.edu>
2018-10-26 04:54:48 -0400

    edited testing phase one

--- Driver.java
+++ Driver.java
@@ -1,53 +1,86 @@
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("\n\n\n*creating SuperArray s");
-		SuperArray s = new SuperArray();
-		System.out.println("print s. (should print: [])");
-		System.out.println("\t" + s);
-		System.out.println("check to see if s is empty. (should print true)\n\t" + s.isEmpty());
-		System.out.println("*Clearing s");
-		s.clear();
-		System.out.println("check to see if s is empty. (should print true) \n\t" + s.isEmpty());
-		System.out.println("*adding 1 to s\n*adding 2 to s");
-		s.add("1");
-		s.add("2");
-		System.out.println("check the size of s. (should print 2)\n\t" + s.size());
-		System.out.println("check to see if s is empty. (should print false)\n\t" + s.isEmpty());
-		System.out.println("printing s. (should print [1, 2])\n\t " + s);
-		//System.out.println(s);
-		System.out.println("print s.toStringDebug(). (should print [1, 2, null, null, null, null, null, null, null, null])\n\t" + s.toStringDebug());
-		//System.out.print(s.toStringDebug());
+public class Driver{
+	public static void main(String[] args){
+		SuperArray beef = new SuperArray();
+		/**phase 1 testing start*/
+		System.out.println("Phase 1 testing, start!");
 		
+		//testing SuperArray initializing
+		System.out.println("\n!!Testing SuperArray initializing!!");
+		System.out.println("Testing toString(): should return []");
+		System.out.println(beef);
+		System.out.println("\nTesting toStringDebug(): should return [null, ... null]");
+		System.out.println(beef.toStringDebug());
 		
-		System.out.println("\n\n---------------------\n*creating SuperArray t");
-		SuperArray t = new SuperArray();
-		System.out.println("t: " + t);
-		System.out.println("t.toStringDebug(): " + t.toStringDebug());
-		System.out.println("*using for loop to add the numbers 0-9 to data");
-		for (int i=0;i<10;i++){
-			t.add(""+i);
+		//testing size()
+		System.out.println("\n\n!!Testing size()!!");
+		System.out.println("Testing size(): should return 0");
+		System.out.println(beef.size());
+		
+		//testing add()
+		System.out.println("\n\n!!Testing add()!!");
+		System.out.println("Testing add() while empty: should return true");
+		System.out.println(beef.add("red"));
+		System.out.println("\nChecking add() and toString(): should return [red]");
+		System.out.println(beef);
+		beef.add("cow");
+		System.out.println("\nChecking add() and toString(): should return [red, cow]");
+		System.out.println(beef);
+		for (int x = 0; x < 9; x++){
+			beef.add("filler");
 		}
-		System.out.println("print t. should print: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\n\t" + t);
-		System.out.println("print t.toStringDebug(). (should print the same thing ^^)\n\t" + t.toStringDebug());
-		System.out.println("checking t.get(int).");
-		System.out.println("t.get(0) should return 0\n\t" + t.get(0));
-		System.out.println("t.get(9) should return 0\n\t" + t.get(9));
-		System.out.println("\nt.get(-1) should return an error then null");
-		System.out.println("\t" +t.get(-1));
-		System.out.println("\nt.get(10) should return error then null");
-		System.out.println("\t"+t.get(10));
+		System.out.println("\n\nTesting add() while full: should return false");
+		System.out.println(beef.add("full"));
 		
-		System.out.println("\nprint t. should be [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\n\t" + t);
-		System.out.println("*set index 4 to \"Good morning\"");
-		System.out.println(t.set(4,"Good morning"));
-		System.out.println("\nprint t. (should be [0, 1, 2, 3, Good morning, 5, 6, 7, 8, 9]\n\t" + t);
-		//System.out.println(t.get(10));
-		//System.out.println(t.get(-1));
-
-
-
+		//testing clear()
+		System.out.println("\n\n!!Testing clear()!!");
+		System.out.println("Testing clear(): should return []");
+		beef.clear();
+		System.out.println(beef);
+		System.out.println("\nTesting clear() and size(): should return 0");
+		System.out.println(beef.size());
+		System.out.println("\nTesting add(\"red\") after clear(): should return true");
+		System.out.println(beef.add("red"));
+		System.out.println("\nSeeing SuperArray: should be [red]");
+		System.out.println(beef);
+		beef.clear();
+		
+		//testing get()
+		System.out.println("\n\n!!Testing get()!!");
+		System.out.println("Testing get() while empty: should return null");
+		System.out.println(beef.get(0));
+		beef.add("red");
+		beef.add("cow");
+		beef.add("meat");
+		System.out.println("\nTesting get() with indices 0-2 filled: should return values *check code*");
+		System.out.println(beef.get(0));
+		System.out.println(beef.get(1));
+		System.out.println(beef.get(2));
+		System.out.println("\nTesting get() for out of bounds: should return null");
+		System.out.println(beef.get(-1));
+		System.out.println(beef.get(10));
+		
+		//testing set()
+		System.out.println("\n\n!!Testing set()!!");
+		System.out.println("Using SuperArray from !!testing get()!!");
+		System.out.println(beef);
+		System.out.println("\nTesting set(0, \"blue\"): should return red");
+		System.out.println(beef.set(0, "blue"));
+		System.out.println("SuperArray should now be [blue, cow, meat]");
+		System.out.println(beef);
+		System.out.println("\nTesting set(1, \"fish\"): should return cow");
+		System.out.println(beef.set(1, "fish"));
+		System.out.println("SuperArray should now be [blue, fish, meat]");
+		System.out.println(beef);
+		System.out.println("\nTesting set(2, \"vegetal\"): should return meat");
+		System.out.println(beef.set(2, "vegetal"));
+		System.out.println("SuperArray should now be [blue, fish, vegetal]");
+		System.out.println(beef);
+		System.out.println("\nUsing empty SuperArray ");
+		beef.clear();
+		System.out.println("\nTesting set(2, \"vegetal\"): should return null");
+		System.out.println(beef.set(2, "vegetal"));
+		System.out.println("SuperArray should now be []");
+		System.out.println(beef);
+		/** phase 1 testing end*/
 	}
-
-
 }
